3e070f48f36181ab087575531ef38c5612a3acc7
weixs <425767281@qq.com>
2018-03-20 13:58:51 +0800

    添加选择框

--- AppMoudle/AppLibrary/src/main/java/com/example/mylibrary/refresh/RefershActivity.java
+++ AppMoudle/AppLibrary/src/main/java/com/example/mylibrary/refresh/RefershActivity.java
@@ -1,14 +1,23 @@
 package com.example.mylibrary.refresh;
 
 import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.LifecycleRegistry;
+import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,6 +31,7 @@ import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Locale;
@@ -34,12 +44,15 @@ import static android.R.layout.simple_list_item_2;
  * Created by wxs on 2018/2/8.
  */
 
-public class RefershActivity extends LifecycleActivity {
+public class RefershActivity extends AppCompatActivity implements LifecycleRegistryOwner {
     SmartRefreshLayout refreshLayout;
     RecyclerView recyclerview;
     Toolbar toolbar;
     private BaseRecyclerAdapter<Void> mAdapter;
     LoadingLayout loading;
+    private final LifecycleRegistry mRegistry = new LifecycleRegistry(this);
+    private SearchView mSearchView;
+    private SearchView.SearchAutoComplete mSearchAutoComplete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +72,28 @@ public class RefershActivity extends LifecycleActivity {
     }
 
     public void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSearchAutoComplete.isShown()) {
+                    try {
+                        mSearchAutoComplete.setText("");
+                        Method method = mSearchView.getClass().getDeclaredMethod("onCloseClicked");
+                        method.setAccessible(true);
+                        method.invoke(mSearchView);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    finish();
+                }
+            }
+        });
+
+
         loading.setEmptyImage(R.drawable.ic_empty);
         loading.showEmpty();
 
@@ -115,8 +150,71 @@ public class RefershActivity extends LifecycleActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.optionsmenu, menu);
+        MenuItem searchItem = menu.findItem(R.id.ab_search);
+
+        //通过MenuItem得到SearchView
+        mSearchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        AppCompatImageView search=(AppCompatImageView)mSearchView.findViewById(android.support.v7.appcompat.R.id.search_button);
+        search.setImageResource(R.drawable.ic_search);
+        AppCompatImageView close=(AppCompatImageView)mSearchView.findViewById(android.support.v7.appcompat.R.id.search_close_btn);
+        close.setImageResource(R.drawable.ic_close);
+        mSearchAutoComplete = (SearchView.SearchAutoComplete) mSearchView.findViewById(R.id.search_src_text);
+        mSearchView.setQueryHint("搜索本地歌曲by code");
+
+        //设置输入框提示文字样式
+        mSearchAutoComplete.setHintTextColor(getResources().getColor(android.R.color.darker_gray));
+        mSearchAutoComplete.setTextColor(getResources().getColor(android.R.color.background_light));
+        mSearchAutoComplete.setTextSize(14);
+        //设置触发查询的最少字符数（默认2个字符才会触发查询）
+        mSearchAutoComplete.setThreshold(1);
+
+        //设置搜索框有字时显示叉叉，无字时隐藏叉叉
+        mSearchView.onActionViewExpanded();
+        mSearchView.setIconified(true);
+
+        //修改搜索框控件间的间隔（这样只是为了更加接近网易云音乐的搜索框）
+        LinearLayout search_edit_frame = (LinearLayout) mSearchView.findViewById(R.id.search_edit_frame);
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) search_edit_frame.getLayoutParams();
+        params.leftMargin = 0;
+        params.rightMargin = 0;
+        search_edit_frame.setLayoutParams(params);
+
+        //监听SearchView的内容
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+//                Cursor cursor = TextUtils.isEmpty(s) ? null : queryData(s);
+//
+////                if (mSearchView.getSuggestionsAdapter() == null) {
+////                    mSearchView.setSuggestionsAdapter(new SimpleCursorAdapter(SearchViewActivity2.this, R.layout.item_layout, cursor, new String[]{"name"}, new int[]{R.id.text1}));
+////                } else {
+////                    mSearchView.getSuggestionsAdapter().changeCursor(cursor);
+////                }
+//                setAdapter(cursor);
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
 
     private Collection<Void> initData() {
         return Arrays.asList(null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null);
+    }
+
+    @Override
+    public LifecycleRegistry getLifecycle() {
+        return mRegistry;
     }
 }
